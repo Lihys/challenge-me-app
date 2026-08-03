8c57b3440fdce549da68e1365100caaf20fd2140
Lihys <lihylevy221b@gmail.com>
2026-08-03 23:19:19 +0300

    testing navigation button from launch to login

--- app/src/main/java/com/course/challengeme/LaunchScreen.kt
+++ app/src/main/java/com/course/challengeme/LaunchScreen.kt
@@ -2,11 +2,13 @@ package com.course.challengeme
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.course.challengeme.navigations.Navigation
 
 @Composable
 fun LaunchScreen(navController: NavController) {
@@ -16,4 +18,8 @@ fun LaunchScreen(navController: NavController) {
     ) {
         Text("Launch Screen")
     }
+    /*Button(onClick = { navController.navigate(Navigation.Login.route) }) {
+        Text("Go to Login")
+    }*/
+
 }
