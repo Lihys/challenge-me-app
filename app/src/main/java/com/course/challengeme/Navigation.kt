065c7a0fdc24e83e50c5896060ee8662a7c99a45
Lihys <lihylevy221b@gmail.com>
2026-08-03 22:33:58 +0300

    screens set up and using composables

--- app/src/main/java/com/course/challengeme/Navigation.kt
+++ app/src/main/java/com/course/challengeme/Navigation.kt
@@ -1,4 +1,20 @@
 package com.course.challengeme
 
-class Navigation {
+sealed class Navigation(val route: String) {
+    object Launch : Navigation("launch")
+    object Login : Navigation("login")
+    object Register : Navigation("register")
+    object Home : Navigation("home")
+    object MyAccount : Navigation("my_account")
+    object CreateChallenge : Navigation("create_challenge")
+    object JoinChallenge : Navigation("join_challenge")
+    object ChallengePage : Navigation("challenge/{challengeId}") {
+        fun createRoute(challengeId: String) = "challenge/$challengeId"
+    }
+    object Leaderboard : Navigation("leaderboard/{challengeId}") {
+        fun createRoute(challengeId: String) = "leaderboard/$challengeId"
+    }
+    object InviteCode : Navigation("invite_code/{challengeId}") {
+        fun createRoute(challengeId: String) = "invite_code/$challengeId"
+    }
 }
